61bea2c1b03b9bd158cfdffb441c613b9e17d2bc
gbochikubo <37846911+gbochikubo@users.noreply.github.com>
2018-05-24 15:31:34 -0300

    Update Conversao.cpp

--- Conversao.cpp
+++ Conversao.cpp
@@ -2,54 +2,59 @@
 #include <cstdlib>
 #include <cmath>
 using namespace std;
-
-void ConverteDecimal(int numero,int* binario, int& tam){
+int ConverteDecimal(int numero, int& tam){
     
     int n = numero;
-    int i = 3;
-    while(n!=0 ){
-        binario[i] = n%2;
-        n = n/2;
-        i--;
-    }
-    int cont = 0;
-    while((binario[0] == 0) and (cont < 3)){
-        for(int i = 0; i < tam-1; i++){
-            binario[i] = binario[i+1];
-        }
-        tam--;
-        cont++;
-    }
-}
-
-
-int ConverteBinario(int *binario, int tam){
-    int cont = tam-1;
     int i = 0;
-    int decimal = 0;
-    while(i < tam ){
-        if(binario[i] == 1){
-            decimal = decimal + pow(2,cont);
-        }
-        cont--;
-        i++;
+    int binario=0;
+    int b[tam];
+    while(n!=0 ){
+        b[i] = n%2;
+        n = n/2;
+        i++;           
     }
-    return decimal;
+    for(int i=0; i < tam; i++){
+		if(b[i] == 1){
+			binario = binario + pow(10,i);
+		}
+	}
+	return binario;
 }
+
+
+int ConverteBinario(int binario){
+    int decimal=0;
+    int copia=binario;
+    int tam = 0;
+    while(copia != 0){
+		copia =copia/10;
+		tam++;
+	}	
+    int b[tam];
+    int i=0;
+    while(binario != 0){
+		if(binario%10 == 0){
+			b[i] = 0;
+		}
+		else{
+			b[i] = 1;
+		}
+		binario = binario/10;
+		i++;
+	}
+	for(int i = 0; i < tam; i++){
+		if(b[i] == 1){
+			decimal = decimal + pow(2,i);
+		}
+	}
+     return decimal;
     
-
-
-
-
+}
 
 int main (){
-    int tam = 4;
-    int* binary = new int[tam];
-    
-    ConverteDecimal(10,binary,tam);
-    cout << ConverteBinario(binary,tam);
-    
-    delete binary;
-    
+
+	int binary = 1011;
+	cout << ConverteBinario(binary) << endl;
+	
     return 0;
 }
